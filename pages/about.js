34268256c088887a33283fa288cde8bb77852978
Andrey Kudinov <2499931@gmail.com>
2022-02-23 23:11:10 +0400

    add link and imports

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,8 @@
+import React from 'react'
+import Link from 'next/link'
+
 export default function About() {
-  return <div>About us</div>
+  return (
+    <div><h1>About us</h1><Link href="/about">about2</Link></div>
+  )
 }
--- pages/about2.js
+++ pages/about2.js
@@ -1,3 +1,8 @@
+import React from 'react'
+import Link from 'next/link'
+
 export default function About2() {
-  return <div>About 2</div>
+  return (
+    <div><h1>About us</h1><Link href="/about">about</Link></div>
+  )
 }
